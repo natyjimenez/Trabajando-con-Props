549ff84f596901473aaae5b6cc034552f8a5d441
Natalia <naty.jimenez@gmail.com>
2020-09-05 17:11:44 -0400

    desafío resuelto

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="contenido">
+    <h1 class="Título">Lista de Tareas</h1>
+    <input class="textInput" v-model="tarea" type="text" name id />
+    <!-- Con el botón llamo a la función agregar, para agregar una tarea mediante el evento click-->
+    <button class="boton" @click="agregar">Agregar Tarea</button>
+    <!-- Título lista de tareas -->
+    <h5 class="títuloLista">Cosas por hacer:</h5>
+    <!-- LLamado al componente tarea, uso de directiva v-for para recorrer el arreglo tareas, y llamado al evento eliminar, para eliminar una tarea -->
+    <tarea
+      v-for="(tarea, i) in tareas"
+      :tarea="tarea"
+      :indice="i"
+      :key="i"
+      @eliminar="eliminar"
+    ></tarea>
+  </div>
+</template>
+
+<script>
+// Importo componente hijo tarea
+import tarea from "./components/Tarea";
+export default {
+  // Nombre del componente
+  nombre: "listaTareas",
+  // Función data retorna un string vacío en que se guardan los datos ingresados en el input
+  // y un array vacío donde se guardan en conjunto los strings, ingresados.
+  data() {
+    return {
+      tarea: "",
+      tareas: [],
+    };
+  },
+  // Referencia al componente hijo "tarea"
+  components: {
+    tarea,
+  },
+  // Métodos
+  methods: {
+    // Función "agregar", mediante el método push, ingreso el string tarea, al array tareas
+    agregar() {
+      this.tareas.push(this.tarea);
+      this.tarea = "";
+    },
+    // Mediante el método splice elimino un objeto (en este caso una tarea), del array tareas
+    eliminar(indice) {
+      this.tareas.splice(indice, 1);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Tarea.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- LLamado a componente tarea -->
+     <ul class="listado">           
+            <li> {{tarea}}</li>
+        </ul>
+    <!-- Botón con un evento click, para borrar una tarea -->
+    <button class="btnBorrar" @click="borrar">Eliminar</button>
+  </div>
+</template>
+
+<script>
+// Componente tarea
+export default {
+  // Nombre del componente
+  name: "tarea",
+  // Propiedades pasadas de componente padre a componente hijo una de tipo string y otra de tipo number
+  props: {
+    tarea: String,
+    indice: Number,
+  },
+  // Propiedades pasadas de componente padre a componente hijo
+  methods: {
+    // Se emite una llamada desde componente hijo con valor eliminar, para ser escuchada por componente padre
+    borrar() {
+      this.$emit("eliminar", this.indice);
+    },
+  },
+};
+</script>
+
+<style></style>
